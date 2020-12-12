d1f5050b1121c0cde96cfa313c55952d616518e2
Ablaihan <ablaihan17771@gmail.com>
2020-12-12 20:11:14 +0600

    commit

---
+++ Clothes_Shop/CustomValid/CustomName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clothes_Shop.CustomValid
+{
+    public class CustomName:ValidationAttribute
+    {
+
+        public override bool IsValid(object value)
+        {
+            String name = value.ToString();
+            return base.IsValid(name);
+        }
+    }
+}
